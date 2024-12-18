e9b75a815c500b0132a5b4857510eaf3028666a4
joshankumar6299 <joshankumar6299@gmail.com>
2024-12-18 20:40:32 +0530

    add FirstProject

---
+++ FirstProject/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from "./Components/header/Header"; 
+import Footer from './Components/footer/Footer'; 
+import Home from './Components/home/Home';
+
+function App() {
+  return (
+    <>
+      <Header /><br />
+     <Home/><br />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
