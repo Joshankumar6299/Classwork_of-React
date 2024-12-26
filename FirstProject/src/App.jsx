7faf14718d0208a84f1372d960c03459a80ad867
joshankumar6299 <joshankumar6299@gmail.com>
2024-12-26 12:37:11 +0530

    add FirstProject

--- FirstProject/src/App.jsx
+++ FirstProject/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from "./Components/header/Header"; 
 import Footer from './Components/footer/Footer'; 
 import Home from './Components/home/Home';
+import Card from './Components/card/Card';
 
 function App() {
   return (
     <>
       <Header />
      <Home/><br />
+     <Card/>
       <Footer />
     </>
   );
