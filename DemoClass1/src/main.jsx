44245115a7a57e4902b7b5431ba125bcb44ba708
joshankumar6299 <joshankumar6299@gmail.com>
2025-01-16 13:23:31 +0530

    add ApiTesting component and comment out existing examples

--- DemoClass1/src/main.jsx
+++ DemoClass1/src/main.jsx
@@ -4,15 +4,17 @@ import './index.css'
 import StateExample from "./Ice2612/StateExamp1"
 import StateMerge from './Ice2612/stateMerge'
 import PropExample from './Ice2612/propExamp1'
+import ApiTesting from './Ice1601/useeffectever'
 
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     
-    <StateExample/>
+    {/* <StateExample/>
     <StateMerge/>
-    <PropExample/>
+    <PropExample/> */}
+    <ApiTesting/>
 
   </StrictMode>,
 )
