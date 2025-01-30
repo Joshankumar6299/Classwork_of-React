97ed29e75ac83a28291f62f4608c38d7a84a3ada
joshankumar6299 <joshankumar6299@gmail.com>
2025-01-30 13:59:36 +0530

    refactor: comment out unused components and import UserContextFinal in main.jsx

--- DemoClass1/src/main.jsx
+++ DemoClass1/src/main.jsx
@@ -1,14 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { UserContext } from './Ice3001/usecontext1'
 // import StateExample from "./Ice2612/StateExamp1"
 // import StateMerge from './Ice2612/stateMerge'
 // import PropExample from './Ice2612/propExamp1'
 // import ApiTesting from './Ice1601/useeffectever'
 // import Exersice from './Ice1601/exersice'
-import ReqCancel from './Ice2201/Classexample'
-import Carouselexe from './Ice2201/Carouselexe'
-import CarouselApp from './Ice2201/Carouselexe'
+// import ReqCancel from './Ice2201/Classexample'
+// import Carouselexe from './Ice2201/Carouselexe'
+// import CarouselApp from './Ice2201/Carouselexe'
+// import UserContext from './Ice3001/usecontext1'
+import UserContextFinal from './Ice3001/usecontext3'
 
 
 
@@ -21,7 +24,8 @@ createRoot(document.getElementById('root')).render(
     {/* <ApiTesting/> */}
     {/* <Exersice/> */}
     {/* <ReqCancel/> */}
-    <CarouselApp/>
+    {/* <CarouselApp/> */}
+   <UserContextFinal/>
 
   </StrictMode>,
 )
